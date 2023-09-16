d8cba0d1a670d028f815edabcc3f09eb9ec2288e
NirukshaSandeepa <121809388+NirukshaSandeepa@users.noreply.github.com>
2023-09-16 14:56:20 +0530

    install and import axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,8 @@
 import './App.css';
-
+import axios from 'axios';
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+  
+  return (<div className="App"> </div>);
 }
 
 export default App;
