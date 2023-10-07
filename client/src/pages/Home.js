459b8b4fef2c1900872331ceee858307f41e2de5
OmalyaV <omalyavid@gmail.com>
2023-10-08 00:16:10 +0530

    done

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import axios from 'axios';
 import { useEffect } from 'react';
+import { useState } from 'react';
+import api from '../apiConfig.js';
 function Home() {
     useEffect(() => {
         axios.get("http://localhost:3002/user.routes.js").then((response) => {
