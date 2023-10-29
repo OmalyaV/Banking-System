5b66aceecb5567838d296db2a7449c66f4f3ec39
LaRasa99 <96112509+LaRasa99@users.noreply.github.com>
2023-10-29 15:56:25 +0530

    Welcome image added

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
 import './App.css';
 function App() {
+<<<<<<< Updated upstream
 
   return (<div className="App"> </div>);
+=======
+  return (
+    <div className="App">
+      <AuthContextProvider>
+        {/* <Account/> */}
+        <Welcome/>
+      </AuthContextProvider>
+    </div>
+  )
+>>>>>>> Stashed changes
 }
 
 export default App;
