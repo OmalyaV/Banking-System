0358db2d29446cf0eb0b3986c9b1bf84c9514460
NirukshaSandeepa <121809388+NirukshaSandeepa@users.noreply.github.com>
2023-10-29 17:18:40 +0530

    modified some css

--- client/src/App.js
+++ client/src/App.js
@@ -11,8 +11,8 @@ function App() {
   return (
     <div className="App">
       <AuthContextProvider>
-        <Account/>
-        {/* <Welcome/> */}
+        {/* <Account/> */}
+        <Welcome/>
       </AuthContextProvider>
     </div>
   )
