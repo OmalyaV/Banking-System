893e61ecbb4cc51cb7cb865857be3e58e5d0cc39
OmalyaV <omalyavid@gmail.com>
2023-10-30 13:20:25 +0530

    done

--- client/src/components/TextInput.jsx
+++ client/src/components/TextInput.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import { TextField } from '@mui/material'
 
-const TextInput = () => {
+const TextInput = ({ onValueChange }) => {
+  const [value, setValue] = React.useState("")
+
+  const handleChange = (event) => {
+    const newValue = event.target.value;
+    setValue(newValue);
+
+    // Call the callback function to update the username in the parent component
+    onValueChange(newValue);
+  };
   return (
     <TextField
+      value={value}
       variant="outlined"
+      onChange={handleChange}
       sx={{
         borderRadius: "50px",
         backgroundColor: "white",
