23f7448908c5b9cc4f73abcf1a0d692d8851077e
OmalyaV <omalyavid@gmail.com>
2023-10-30 09:57:03 +0530

    done

---
+++ employee_client/src/pages/Welcome/NewsBar.jsx
@@ -0,0 +1,58 @@
+import React from "react"
+import { Stack, Box, Typography, Button } from "@mui/material"
+
+const NewsBar = () => {
+  return (
+    <div
+      class="image"
+      style={{
+        height: "550px",
+        width: "100%",
+        backgroundImage: 'url("assets/images/welcome_image.png")',
+        backgroundSize: "contain",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <Stack direction="row" spacing={"100px"} justifyContent="center">
+        <Stack direction="column" spacing={"0px"} textAlign="left">
+          <Typography fontSize={"72px"}>BankerLink : Fostering</Typography>
+          <Typography fontSize={"72px"}>Collaboration, Wellness,</Typography>
+          <Typography fontSize={"72px"}>and Success</Typography>
+          <Box width={"500px"}>
+            <Typography fontSize={"15px"}>
+              Welcome to NexusTrust Bank, where your professional journey
+              begins. Dive into our wide array of opportunities, expert
+              mentorship, and innovative solutions for a path towards career
+              growth and job security. Here at NexusTrust Bank, we are committed
+              to providing our employees with a supportive and dynamic
+              environment, enabling them to reach their full potential while
+              contributing to our shared success. Join us on this exciting
+              professional expedition, and let's build a prosperous future
+              together..
+            </Typography>
+          </Box>
+        </Stack>
+        <Button
+          variant="contained"
+          sx={{
+            width: "300px",
+            height: "50px",
+            fontFamily: "Inter",
+            textTransform: "none",
+            fontWeight: 600,
+            backgroundColor: "#FFCF43",
+            color: "black",
+            "&:hover": {
+              backgroundColor: "#DDCC40", // Specify the hover color
+            },
+            borderRadius: "10px", // Add this to round the button edges
+          }}
+        >
+          Employee Login
+        </Button>
+      </Stack>
+    </div>
+  )
+}
+
+export default NewsBar
---
+++ employee_client/src/popups/AboutUs.jsx
@@ -0,0 +1,29 @@
+import React from "react"
+import { Box, Dialog, Typography } from "@mui/material"
+
+const AboutUsPopup = (props) => {
+  const { onClose, open, name } = props
+
+  const handleClose = () => {
+    onClose(name)
+  }
+
+  return (
+    <Dialog open={open} onClose={handleClose}>
+      <Box
+        sx={{
+          backgroundColor: "grey",
+          margin: 0.1,
+          padding: 0, // Set the background color to black
+          boxShadow: "none", // Remove the shadow
+        }}
+        alignItems={"center"}
+        flex={"row"}
+      >
+        <Typography>{name}</Typography>
+      </Box>
+    </Dialog>
+  )
+}
+
+export default AboutUsPopup
