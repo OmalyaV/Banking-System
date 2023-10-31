e83864d31481f87c7d4619d19955eb96e8a7449f
Shalani Kuruppu <adeeshak.21@cse.mrt.ac.lk>
2023-11-01 01:22:24 +0530

    done

--- client/src/pages/Account/LoansFD.jsx
+++ client/src/pages/Account/LoansFD.jsx
@@ -12,7 +12,7 @@ const Loans = () => {
   const accountType = "Adult"
 
   return (
-    <Stack direction="row" spacing={5} justifyContent="center">
+    <Stack direction="row" spacing={20} >
       <Stack spacing={0}>
         <Box textAlign="left" sx={{ padding: "20px 150px" }}>
           {/* Left Side */}
@@ -38,22 +38,15 @@ const Loans = () => {
           </Typography>
           <Stack padding={{ paddingTop: "10px" }} direction="column" spacing={2}>
             <Box>
-            <Typography
-            sx={{
-              color: "#FFCF43",
-              fontSize: 20,
-              fontWeight: 500,
-            }}
-          >
-            Fixed Deposits
-          </Typography>
+           
               </Box>
             <Box>
-              <Typography
+             
+               <Typography
                 sx={{
                   color: "white",
-                  fontSize: 18,
-                  fontWeight: "bold",
+                  fontSize: 12,
+                  fontWeight: 400,
                   padding: "0px 0px",
                 }}
                 fontFamily={"Inter"}
@@ -75,11 +68,12 @@ const Loans = () => {
           </Stack>
           <Stack padding={{ paddingTop: "20px" }} direction="row" spacing={3}>
             <Box>
+            
               <Typography
                 sx={{
                   color: "white",
-                  fontSize: 18,
-                  fontWeight: "bold",
+                  fontSize: 12,
+                  fontWeight: 400,
                   padding: "0px 0px",
                 }}
                 fontFamily={"Inter"}
@@ -88,27 +82,37 @@ const Loans = () => {
               </Typography>
               <GreyBox>
 
-                <Typography font family={"Inter"} style={{ textAlign: "left" }}>Plan Basic : For 6 months at interest of 13%</Typography>
-                <Typography font family={"Inter"} style={{ textAlign: "left" }}>Plan Basic : For 1 year at interest of 14%</Typography>
-                <Typography font family={"Inter"} style={{ textAlign: "left" }}>Plan Basic : For 3 years at interest of 15%</Typography>
+              
+                <GreyBox>
+                 <Typography fontFamily="Inter" style={{ textAlign: "left" }}>
+                 1 year   -14%      $30,000.00
+                 </Typography>
+              <Typography fontFamily="Inter" style={{ textAlign: "left" }}>
+               6 months   -13%    $30,000.00
+              </Typography>
+            <Typography fontFamily="Inter" style={{ textAlign: "left" }}>
+                  3 months   -12%    $30,000.00
+            </Typography>
+          </GreyBox>
 
               </GreyBox>
             </Box>
           </Stack>
 
         </Box>
-        x
+        
         
       </Stack>
 
       
        
         <Stack spacing={0}>
-
+        <stack>
+          
         <Typography
           fontFamily={"Inter"}
           color={"white"}
-          padding={{ paddingBottom: "20px" }}
+          padding={{ paddingBottom: "10px" }}
         >
 
           My Loans
@@ -118,116 +122,17 @@ const Loans = () => {
           <Typography fontFamily={"Inter"}>Loan 2</Typography>
           <Typography fontFamily={"Inter"}>Loan 3</Typography>
         </GreyBox>
-      
+        <Box>
+        <GreyBox>
+        <Typography
+          fontFamily={"Inter"}
+          color={"white"}
+          padding={{ paddingBottom: "20px" }}
+        >
 
-        <Box padding={{ paddingTop: "20px" }}>
-            <Typography
-              sx={{
-                color: "white",
-                fontSize: 18,
-                fontWeight: "bold",
-                padding: "0px 0px",
-              }}
-              fontFamily={"Inter"}
-            >
-              Online Loan Request
-            </Typography>
-          </Box>
-        <Box sx={{ paddingLeft: "50px" }}>
-          <Paper
-            sx={{
-              paddingLeft: "50px",
-              backgroundColor: "#151515",
-              textAlign: "left",
-              borderRadius: "20px",
-              width: "500px",
-            }}
-          >
-            <Grid container spacing={2}>
-              <Grid item xs={6}>
-                <Typography
-                  sx={{
-                    color: "white",
-                    fontWeight: 400,
-                    padding: "0px 0px",
-                  }}
-                  fontFamily={"Inter"}
-                >
-                  FD:
-                </Typography>
-              </Grid>
-              <Grid item xs={6}>
-                <TextField
-                  variant="outlined"
-                  sx={{
-                    borderRadius: "50px",
-                    backgroundColor: "white",
-                    "& fieldset": { border: "none" },
-                    "& input": {
-                      paddingTop: "6px",
-                      paddingBottom: "6px",
-                    },
-                  }}
-                />
-              </Grid>
-              <Grid item xs={6}>
-                <Typography
-                  sx={{
-                    color: "white",
-                    fontWeight: 400,
-                    padding: "0px 0px",
-                  }}
-                  fontFamily={"Inter"}
-                >
-                  Amount:
-                </Typography>
-              </Grid>
-              <Grid item xs={6}>
-                <TextField
-                  variant="outlined"
-                  sx={{
-                    borderRadius: "50px",
-                    backgroundColor: "white",
-                    "& fieldset": { border: "none" },
-                    "& input": {
-                      paddingTop: "6px",
-                      paddingBottom: "6px",
-                    },
-                  }}
-                />
-              </Grid>
-
-              <Grid item xs={6}>
-                <Typography
-                  sx={{
-                    color: "white",
-                    fontWeight: 400,
-                    padding: "0px 0px",
-                  }}
-                  fontFamily={"Inter"}
-                >
-                  Time Period
-                  (in months):
-                </Typography>
-              </Grid>
-              <Grid item xs={6}>
-                <TextField
-                  variant="outlined"
-                  sx={{
-                    borderRadius: "50px",
-                    backgroundColor: "white",
-                    "& fieldset": { border: "none" },
-                    "& input": {
-                      paddingTop: "6px",
-                      paddingBottom: "6px",
-                    },
-                  }}
-                />
-              </Grid>
-
-            </Grid>
-            <Box sx={{ padding: "10px 0px", borderRadius: "20px" }}>
-              <Button
+          Online Loan Request
+        </Typography>
+        <Button
                 variant="contained"
                 sx={{
                   fontFamily: "Inter",
@@ -241,10 +146,15 @@ const Loans = () => {
                   borderRadius: "20px", // Add this to round the button edges
                 }}
               >
-                Proceed
+                Select FD
               </Button>
-            </Box>
-          </Paper>
+        </GreyBox>
+        </Box>
+        </stack>
+
+        
+        <Box sx={{ paddingLeft: "50px" }}>
+         
         </Box>
         
         
