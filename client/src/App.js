c1bb0570403cfc798c56e13cbb46bc97c8e4c0b2
OmalyaV <omalyavid@gmail.com>
2023-10-31 19:02:28 +0530

    cookies implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,49 @@
 import "./App.css"
+import React from "react"
 import axios from "axios"
 import LoginPopup from "./popups/Login"
-
-
 import Account from "./pages/Account"
 import Welcome from "./pages/Welcome"
 import AuthContextProvider, { AuthContext } from "./context/AuthContext"
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
-import { AccountContext } from "./context/AccountContext"
 import AccountContextProvider from "./context/AccountContext"
+import { Navigate } from 'react-router-dom'
+import Cookies from "universal-cookie";
 
 function App() {
+  const { user,username, userType, login, logout } = React.useContext(AuthContext)
+  const cookies = new Cookies();
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+    if (cookies.get("user") !== undefined) {
+      await login(cookies.get("user").user)
+      console.log("Cookie",cookies.get("user").user)
+    }
+    console.log("usertype",userType)
+  }
+  fetchData()
+  },[])
+
   return (
     <div className="App">
-      <AuthContextProvider>
         <Router>
       <Routes>
         <Route path="/" element={<Welcome />} />
         </Routes>
           <AccountContextProvider>
         <Routes>
+          
+        
             <Route path="/account" element={<Account />} />
 
-        </Routes>
+           </Routes>
           </AccountContextProvider>
         {/* <Welcome /> */}
         </Router>
-      </AuthContextProvider>
     </div>
   )
 }
+
 
 export default App
